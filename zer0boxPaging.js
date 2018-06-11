2292830c09b80ad08da77298e772e5f58211f5a5
kim, ji-hoon <zer0box@naver.com>
2018-06-11 12:45:16 +0900

    prev, next 부분 수정

--- zer0boxPaging.js
+++ zer0boxPaging.js
@@ -1,7 +1,8 @@
 
     $.fn.zer0boxPaging = function( opt ) {
-        var option = $.extend({"default":"none"}, opt );//외부로 부터 받아올 옵션
 
+        var option = $.extend({"default":"none"}, opt );//외부로 부터 받아올 옵션
+        var content = "";
         //li 태그의 스타일  
         var liCss = {
             "display":"inline",
@@ -14,18 +15,20 @@
         };
 
         this.empty();//한번 지우고
+
         var start = 1;
-        var end = 5;
+        var end = 5;  
+        //최초 리스트 생성
+        this.append("<ul id='pagingList' style='list-style:none; margin:10px'><li page='#'>Prev</li></ul>");
         
-        var content = "<ul style='list-style:none; margin:10px'>";
-				
+        //이전 페이지가 있을 경우 속성 추가
         if(option.currPage > option.viewRange){          	
 			end = Math.ceil(option.currPage/option.viewRange)*option.viewRange;
-			start = end-(option.viewRange-1);       
-	        console.log("plugin",start+" ~ "+end);
-			content +="<li page='"+(start-1)+"'>Prev</li>";
+            start = end-(option.viewRange-1);      
+            $("#pagingList li").first().attr("page", start-1);
         }
-		
+        
+		//페이징 본문
         for(var i = start; i<=end;i++){
             if(i <= option.maxPage){
                 if(option.currPage == i){
@@ -36,19 +39,34 @@
     
             }
         }
-		
+
+        //다음 페이지 추가
+        content +="<li page='#'>Next</li>";
+        $("#pagingList").append(content);
+
+        //다음 페이지가 있을 경우 속성 추가
         if(end < option.maxPage){
-            content +="<li page='"+(end+1)+"'>Next</li>"
-        }
-    
-        content += "</ul>"
-        this.append(content);
+            $("#pagingList li").last().attr("page", end+1);
+        }            
+        
         var li = this.find("li");
+        var clickPage = 0;     
+
+
+        //이벤트 추가
         li.on("click",option.clickAction);
+        
+        //마우스 오버 추가
         li.hover(function(){
             $(this).css("border-color","red");
         },function(){
             $(this).css("border-color","gray");
         });
+        //스타일 적용
         li.css(liCss);
+        //prev 나 next 의 page 속성이 # 일경우...
+        $("#pagingList").find("li[page='#']").each(function(){
+            $(this).css("color","gray");
+            $(this).css("cursor","default");
+        });
     };
